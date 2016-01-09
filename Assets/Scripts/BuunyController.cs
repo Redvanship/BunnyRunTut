9cff6115109249408f5c54109dba51933e87900f
Redvanship <jaimeveltens@gmail.com>
2016-01-09 12:32:23 +0100

    Added bunny animations

--- Assets/Scripts/BuunyController.cs
+++ Assets/Scripts/BuunyController.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class BuunyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
+    private Animator myAnim;
     public float bunnyJumpForce = 500f;
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -16,5 +18,6 @@ public class BuunyController : MonoBehaviour {
         {
             myRigidBody.AddForce(transform.up * bunnyJumpForce);
         }
-	}
+        myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
+    }
 }
